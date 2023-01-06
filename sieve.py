b7fd9c62bf5eae99553d0464cd9183dc5a822819
Oluwajuwon Oyalude <oyaludejuwon@yahoo.com>
2023-01-06 06:28:37 +0100

    added sieve lab

---
+++ sieve.py
@@ -0,0 +1,11 @@
+﻿my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+my_list2 = []
+#
+# Write your code here.
+#
+for i in my_list:
+    if i not in my_list2:
+        my_list2.append(i)
+my_list = my_list2[:]
+print("The list with unique elements only:")
+print(my_list)
