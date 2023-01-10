5bde0de56029ea6316faf678701ca98585e0c23a
Oluwajuwon Oyalude <oyaludejuwon@yahoo.com>
2023-01-10 15:47:20 +0100

    added new labs and renamed a file

--- hypothesis.py
+++ hypothesis.py
@@ -1,8 +1,19 @@
-c = 16
-step = 0
-while c > 0:
-    c //= 2
-    step += 1
-    if c < 1:   break
-    print(c)
-print('step:',step)
+c0 = int(input("Enter c0: "))
+
+if c0 > 1:
+    # steps = 0
+	# The while loop goes here.
+    while c0 != 1:
+        if c0 % 2 != 0:
+			# Write your code here.
+            cnew = 3 * c0 + 1
+        else:
+            cnew = c0 // 2
+           
+            print(cnew)
+		# 
+		# Write your code here.
+		# 
+# 	print("steps =",steps)
+else:
+	print("Bad c0 value")
---
+++ prime numbers.py
@@ -0,0 +1,26 @@
+﻿def is_prime(num):
+#
+# Write your code here.
+#
+    for i in range(2,num):
+        if num % i == 0:
+            return False
+            break
+    else:
+        return True
+    
+for i in range(1, 20):
+	if is_prime(i + 1):
+			print(i + 1, end=" ")
+print()
+
+
+# num = 9
+
+# for i in range(2,num):
+#     if num % i == 0:
+#         print(False)
+#         break
+# else:
+#     print(True)
+    
