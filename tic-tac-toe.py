055e50d69635190d8e6861171cf94763edd2d89d
Oluwajuwon-O <oyaludejuwon@yahoo.com>
2024-01-02 20:52:23 +0100

    display board func

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,37 +1,24 @@
 
-# these are two ways to create the board
-
+board = [[1,2,3],[4,5,6],[7,8,9]]
 
 # my own solution
-def display_board():
-    digits = [[1,2,3],[4,5,6],[7,8,9]]
-    board = f"""      
+def display_board(board):
+    print(
+    f"""      
     +-------+-------+-------+
     |       |       |       |
-    |   {digits[0][0]}   |   {digits[0][1]}   |   {digits[0][2]}   |
-    |       |       |       |
-    +-------+-------+-------+
-    |       |       |       |
-    |   {digits[1][0]}   |   {digits[1][1]}   |   {digits[1][2]}   |
+    |   {board[0][0]}   |   {board[0][1]}   |   {board[0][2]}   |
     |       |       |       |
     +-------+-------+-------+
     |       |       |       |
-    |   {digits[2][0]}   |   {digits[2][1]}   |   {digits[2][2]}   |
+    |   {board[1][0]}   |   {board[1][1]}   |   {board[1][2]}   |
+    |       |       |       |
+    +-------+-------+-------+
+    |       |       |       |
+    |   {board[2][0]}   |   {board[2][1]}   |   {board[2][2]}   |
     |       |       |       |
     +-------+-------+-------+
     """
-    print(board)
+    )
 
-display_board()
-
-
-# cisco solution
-board = [ [3 * j + i + 1 for i in range(3)] for j in range(3) ]
-print("+-------" * 3,"+", sep="")
-for row in range(3):
-    print("|       " * 3,"|", sep="")
-    for col in range(3):
-        print("|   " + str(board[row][col]) + "   ", end="")
-    print("|")
-    print("|       " * 3,"|",sep="")
-    print("+-------" * 3,"+",sep="")
+display_board(board)
