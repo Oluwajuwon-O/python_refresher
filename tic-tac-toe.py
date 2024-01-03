ad22a4e045e1bea40bb5ad404ed970c6e2cf7206
Oluwajuwon-O <oyaludejuwon@yahoo.com>
2024-01-03 22:57:04 +0100

    don't know what i'm doing

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,5 +1,14 @@
 
 board = [[1,2,3],[4,5,6],[7,8,9]]
+a = board[0][0]
+b = board[0][1]
+c = board[0][2]
+d = board[1][0]
+e = board[1][1]
+f = board[1][2]
+g = board[2][0]
+h = board[2][1]
+i = board[2][2]
 
 # my own solution
 def display_board(board):
@@ -7,18 +16,23 @@ def display_board(board):
     f"""      
     +-------+-------+-------+
     |       |       |       |
-    |   {board[0][0]}   |   {board[0][1]}   |   {board[0][2]}   |
+    |   {a}   |   {b}   |   {c}   |
     |       |       |       |
     +-------+-------+-------+
     |       |       |       |
-    |   {board[1][0]}   |   {board[1][1]}   |   {board[1][2]}   |
+    |   {d}   |   {e}   |   {f}   |
     |       |       |       |
     +-------+-------+-------+
     |       |       |       |
-    |   {board[2][0]}   |   {board[2][1]}   |   {board[2][2]}   |
+    |   {g}   |   {h}   |   {i}   |
     |       |       |       |
     +-------+-------+-------+
     """
     )
 
-display_board(board)
+
+def enter_move(board):
+    # The function accepts the board's current status, asks the user about their move,
+    # checks the input, and updates the board according to the user's decision.
+
+        
